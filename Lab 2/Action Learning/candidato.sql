aba6382e4d3704d1f7aacadec04af8e028f195e8
Antonio Blanco <eggemplo@gmail.com>
2023-11-21 20:48:36 +0100

    action learning Lab 2

---
+++ Lab 2/Action Learning/candidato.sql
@@ -0,0 +1,33 @@
+CREATE TABLE skill (
+	id BIGINT(20) AUTO_INCREMENT,
+	nombre VARCHAR(128),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE idioma (
+	id BIGINT(20) AUTO_INCREMENT,
+	nombre VARCHAR(128),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE candidato (
+	id BIGINT(20) AUTO_INCREMENT,
+	nombre VARCHAR(128),
+	apellidos VARCHAR(256),
+	avatar VARCHAR(512),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE candidato_skill (
+	id_candidato BIGINT(20),
+	id_skill BIGINT(20),
+	nivel VARCHAR(128),
+	PRIMARY KEY (id_candidato,id_skill)
+);
+
+CREATE TABLE candidato_idioma (
+	id_candidato BIGINT(20),
+	id_idioma BIGINT(20),
+	nivel VARCHAR(128),
+	PRIMARY KEY (id_candidato,id_idioma)
+);
